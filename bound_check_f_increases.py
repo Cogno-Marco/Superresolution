59d042e6aed6b495659e0a14fa22215f7049070f
Cogno-Marco <marco.cognolato.98@gmail.com>
2021-10-02 13:40:04 +0200

    Implemented first graph to check if p decrease as f increases

--- bound_check_f_increases.py
+++ bound_check_f_increases.py
@@ -1,5 +1,7 @@
 from general.world_1d import World_1d
 from general.photo_1d import Photo_1d
+import matplotlib.pyplot as plt
+import numpy as np
 
 # registration algorithm:
 # compare each macropixel of a photo with a macropixel of the world at an offset
@@ -11,9 +13,11 @@ from general.photo_1d import Photo_1d
 # 1. probability at the offset where the photo was taken > probability at other offsets
 # 2. as k increases we're less likely to guess a wrong offset compared to the one where the photo was actually taken
 
+# TODO: probability is *really* small, better work with ints than with floats
+
 # world setup
-k = 5
-r = 2
+k = 100
+r = 4
 f = 0
 world = World_1d(2 * k * r)
 
@@ -31,3 +35,22 @@ for i in range(0, r+1):
     probabilities.append(probability)
 
 print(probabilities)
+
+guess = probabilities.index(max(probabilities))
+print(f"guessed position: {guess} actual offset: {photo.offset}")
+
+# Data for plotting
+t = [i for i in range(r+1)]
+s = probabilities
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='offsets (f)', ylabel='probability (p)',
+       title='Probability check of a single photo')
+ax.grid()
+
+plt.ylim(bottom=0)
+
+# fig.savefig("test.png")
+plt.show()
